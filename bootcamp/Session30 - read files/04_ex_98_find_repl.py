5e31aef0e3513e7344f124b933d21c3aa78fc671
douglasmtome <95723028+douglasmtome@users.noreply.github.com>
2022-05-19 20:20:56 -0300

    Terminar Challenges

--- bootcamp/Session30 - read files/04_ex_98_find_repl.py
+++ bootcamp/Session30 - read files/04_ex_98_find_repl.py
@@ -1,22 +1,21 @@
 '''
-statistics('story.txt') 
-# {'lines': 172, 'words': 2145, 'characters': 11227}
+find_and_replace('story.txt', 'Alice', 'Colt') 
+# story.txt now contains the first chapter of my new book,
+# Colt's Adventures in Wonderland
 '''
+
 #BUILDING!
 def find_and_replace(file_in, word_ori, new_word):    
-    with open(file_in) as file:                
+    with open(file_in, 'r') as file:                
         file_read = file.read() 
-        file_words = file_read.replace(word_ori,new_word)
+    
+    file_read = file_read.replace(word_ori,new_word)
 
-    return file_words
+    with open(file_in, 'w') as file:
+        file.write(file_read)
+
+    return None
         
-print(find_and_replace('/Users/douglastome/03-Python-bootcamp/Session30/copy/story.txt', 'Alice', 'NEEEW'))
+print(find_and_replace('copy/story.txt', 'Alice', 'NEEEW'))
 
 #UDEMY
-def statistics(file_name):
-    with open(file_name) as file:
-        lines = file.readlines()
- 
-    return { "lines": len(lines),
-             "words": sum(len(line.split(" ")) for line in lines),
-             "characters": sum(len(line) for line in lines) }
